0dc3fb4fd960bb7e9ee2e4e6a0d0a6d838bf62dd
root <josagoor@gmail.com>
2025-05-13 09:14:52 -0500

    3er Intento

--- presentacion/sesionMedico.php
+++ presentacion/sesionMedico.php
@@ -5,3 +5,10 @@ $medico -> consultar();
 echo "Hola " . $medico -> getNombre() . " " . $medico -> getApellido();
 echo "Usted tiene la especialidad: " . $medico -> getEspecialidad() -> getNombre();
 ?>
+<?php
+    include("Cabeza.php");
+?>
+    
+<?php
+    include("Pie.php");
+?>
